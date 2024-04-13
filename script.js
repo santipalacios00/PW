63ed9a159a4402e3da7bfcd62c0e61eb7048dfa1
spalacios00 <santipalacios13@gmail.com>
2024-04-13 17:03:24 -0300

    ALF-random

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Array de URLs de videos o fotos aleatorias de Alf
+    var alfVideos = [
+        "https://www.youtube.com/watch?v=2JlnDtLw_AE&ab_channel=HacKan",
+        "https://www.youtube.com/watch?v=2hBSMInlDGg&ab_channel=JanSchmelter",
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+        // Agrega más URLs de videos o fotos según sea necesario
+    ];
+
+    // Obtener todos los elementos <a> dentro de <li> con la clase "random-header"
+    var randomLinks = document.querySelectorAll(".random-header a");
+
+    // Asignar una URL aleatoria a cada enlace
+    randomLinks.forEach(function(link) {
+        // Generar un índice aleatorio para seleccionar una URL del array
+        var randomIndex = Math.floor(Math.random() * alfVideos.length);
+        // Crear una URL con un parámetro de consulta único
+        var randomUrl = alfVideos[randomIndex] + "?cachebuster=" + Date.now();
+        // Asignar la URL aleatoria al atributo "href" del enlace
+        link.href = randomUrl;
+    });
+});
+
+
