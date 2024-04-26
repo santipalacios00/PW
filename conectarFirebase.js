da4eb9c5cb03f944da7ba734c043566e76649113
spalacios00 <santipalacios13@gmail.com>
2024-04-26 18:54:12 -0300

    commit

---
+++ conectarFirebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFKo65veH6H_NfZEPEaVRqPv-DtBwGWxM",
+    authDomain: "alfgame-e438f.firebaseapp.com",
+    projectId: "alfgame-e438f",
+    storageBucket: "alfgame-e438f.appspot.com",
+    messagingSenderId: "493104096636",
+    appId: "1:493104096636:web:1361d0bdda78a2ff5af0f9",
+    measurementId: "G-EHQ8FJ9J2F"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app); // Obtén la instancia de Firestore
+
+console.log("Conexión a Firebase establecida correctamente.");
--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBFKo65veH6H_NfZEPEaVRqPv-DtBwGWxM",
-    authDomain: "alfgame-e438f.firebaseapp.com",
-    projectId: "alfgame-e438f",
-    storageBucket: "alfgame-e438f.appspot.com",
-    messagingSenderId: "493104096636",
-    appId: "1:493104096636:web:1361d0bdda78a2ff5af0f9",
-    measurementId: "G-EHQ8FJ9J2F"
-};
-    
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 // Obtener una referencia a la colección "Frases"
 const db = firebase.firestore();
 const frasesCollection = db.collection("Frases");
-
 // Función para obtener la frase del documento con ID "1" de la colección "Frases"
 function obtenerFrase() {
     // Obtener el documento con ID "1" de la colección "Frases"
