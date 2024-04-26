b880923340ac4d82c9f3a9a96dc1468c6c5df400
spalacios00 <santipalacios13@gmail.com>
2024-04-26 18:40:42 -0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFKo65veH6H_NfZEPEaVRqPv-DtBwGWxM",
+    authDomain: "alfgame-e438f.firebaseapp.com",
+    projectId: "alfgame-e438f",
+    storageBucket: "alfgame-e438f.appspot.com",
+    messagingSenderId: "493104096636",
+    appId: "1:493104096636:web:1361d0bdda78a2ff5af0f9",
+    measurementId: "G-EHQ8FJ9J2F"
+};
+    
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+// Obtener una referencia a la colección "Frases"
+const db = firebase.firestore();
+const frasesCollection = db.collection("Frases");
 
+// Función para obtener la frase del documento con ID "1" de la colección "Frases"
+function obtenerFrase() {
+    // Obtener el documento con ID "1" de la colección "Frases"
+    frasesCollection.doc("1").get().then((doc) => {
+        if (doc.exists) {
+            // Obtener los datos del documento
+            const fraseData = doc.data();
 
+            // Actualizar el contenido del elemento "phrase" en el HTML con la frase obtenida
+            const phraseElement = document.querySelector(".phrase");
+            phraseElement.textContent = fraseData.frase;
+        } else {
+            console.log("No se encontró el documento con ID '1'");
+        }
+    }).catch((error) => {
+        console.error("Error al obtener la frase:", error);
+    });
+}
+
+// Llamar a la función obtenerFrase al iniciar el juego
+window.addEventListener("DOMContentLoaded", obtenerFrase);
 
 
 const audio = document.getElementById('miAudio');
