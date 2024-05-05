6ffd234e2db7c86a64f2fd9a503356a81fef8c28
spalacios00 <santipalacios13@gmail.com>
2024-05-05 18:15:06 -0300

    mostrar ranking

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getDocs, query, collection, orderBy } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFKo65veH6H_NfZEPEaVRqPv-DtBwGWxM",
+    authDomain: "alfgame-e438f.firebaseapp.com",
+    projectId: "alfgame-e438f",
+    storageBucket: "alfgame-e438f.appspot.com",
+    messagingSenderId: "493104096636",
+    appId: "1:493104096636:web:1361d0bdda78a2ff5af0f9",
+    measurementId: "G-EHQ8FJ9J2F"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 
 // Tu código aquí
 
